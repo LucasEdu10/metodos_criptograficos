06d6ded80f68a799d063b28de3a657046d29af6d
Lucas Eduardo <lucas@genomika.com.br>
2020-09-17 11:13:34 -0300

    add loop for menu

--- metodo_cesar/cesar.py
+++ metodo_cesar/cesar.py
@@ -1,4 +1,3 @@
-
 convertido2 = ''
 
 def cripto():
@@ -30,18 +29,22 @@ def dscripto():
     print('A palavra descriptografada é: ',convertido)
 
 def menu():
-    print('|========================|')
-    print('| [1] - Encriptografar   |')
-    print('| [2] - Descriptografar  |')
-    print('| [0] - Encerrar         |')
-    print('|========================|')
-    print('')
-    opc = int(input('Digite uma opção: '))
-    if opc == 1:
-        cripto()
-    elif opc == 2:
-        dscripto()
-    else:
-        print('Até a proxima!')
+    check = True
+    while(check):
+        print('')
+        print('|========================|')
+        print('| [1] - Encriptografar   |')
+        print('| [2] - Descriptografar  |')
+        print('| [0] - Encerrar         |')
+        print('|========================|')
+        print('')
+        opc = int(input('Digite uma opção: '))
+        if opc == 1:
+            cripto()
+        elif opc == 2:
+            dscripto()
+        else:
+            check = False
+            print('Até a proxima!')
 
 menu()
